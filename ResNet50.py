c7a03d932a35f61af18e36c2e2113d0a39749550
Vladimir <erkulev@mail.ru>
2021-06-05 11:52:43 +1000

    1

--- ResNet50.py
+++ ResNet50.py
@@ -13,7 +13,7 @@ import numpy as np
 
 model = ResNet50(weights='imagenet')
 
-img_path = 'm.jpg'
+img_path = 'data/m.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
